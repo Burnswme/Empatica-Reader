937bff9542e860617878291a089dd4d5e6c9f8ba
Promethicon <Promethicon@gmail.com>
2017-04-11 19:37:14 -0400

    Update loadusrdata.py

--- loadusrdata.py
+++ loadusrdata.py
@@ -3,31 +3,31 @@ from tkinter import *
 from tkinter import ttk
 import pymysql
 
+
 class loadDataGUI():
     # establish db connection to be used
     connection = pymysql.connect(host='localhost',
-                                      user='root',
-                                      password='',
-                                      db='empaticareader')
+                                 user='root',
+                                 password='',
+                                 db='empaticareader')
 
     cursor = connection.cursor()
 
     # an offset to put utc time into eastern time, 4 hours
-    offset = 3600*4
+    offset = 3600 * 4
 
     def __init__(self):
         self.datawin = Tk()
 
         self.datawin.title('Empatica Reader')
 
-        self.loadButton = ttk.Button(self.datawin, text = 'choose file for upload',command = self.open)\
-            .place(relx = .5, rely = .5, anchor = "center")
+        self.loadButton = ttk.Button(self.datawin, text='choose file for upload', command=self.open) \
+            .place(relx=.5, rely=.5, anchor="center")
 
-        self.fin = ttk.Button(self.datawin, text = 'done', command = self.close)\
-            .place(relx = .5, rely = .8, anchor = "center")
+        self.fin = ttk.Button(self.datawin, text='done', command=self.close) \
+            .place(relx=.5, rely=.8, anchor="center")
 
         self.datawin.mainloop()
-
 
     def close(self):
         self.datawin.destroy()
@@ -42,65 +42,61 @@ class loadDataGUI():
         self.accarr = self.activity.read().split("\n")
         self.arousalarr = self.arousal.read().split("\n")
 
-        #to put timestamp to next lowest hour
-        stime = int(float(self.hrarr[0]))%3600
-        stime = int(float(self.hrarr[0]))-stime+3600
+        # to put timestamp to next lowest hour
+        stime = int(float(self.hrarr[0])) % 3600
+        stime = int(float(self.hrarr[0])) - stime + 3600
         stime = stime - self.offset
 
-        #fills any missing dates with 0
+        # fills any missing dates with 0
         self.fill(self.hrarr[0])
-        
-        etime = len(self.hrarr)//3600
 
-        while stime<=etime:
-            self.cursor.execute('INSERT into Data(date) VALUES ('+str(stime)+')')
+        etime = len(self.hrarr) // 3600
+
+        while stime <= etime:
+            self.cursor.execute('INSERT into Data(date) VALUES (' + str(stime) + ')')
             stime += 3600
 
         # call the methods to compile the data from the files
         self.dbavger(self.hrarr)
         self.dbavger(self.arousalarr)
 
-
     # method to fill missing rows of db
     def fill(self, dat):
 
         # pull largest and thus most recent date from db
-        self.cursor.exectute('SELECT MAX(date) FROM Data')
+        self.cursor.execute('SELECT MAX(date) FROM Data')
         last = self.cursor.fetchone()
-        last -= last(0)-self.offset
+        num = last[0]
+        num -= int(num) - self.offset
         i = 1
         # fill the days with no data with 0's
-        while(float(dat)< last):
-            last = (last+3600*i)
-            stat = 'INSERT into Data (date, ACC, HR, EDA) VALUES('+str(last)+', 0,0,0)'
+        while (float(dat) < num):
+            num = (last + 3600 * i)
+            stat = 'INSERT into Data (date, ACC, HR, EDA) VALUES(' + str(num) + ', 0,0,0)'
             self.cursor.execute(stat)
-
 
     # commits data to data base
     def commitdb(self, ar, st):
-        i=0
+        i = 0
 
-
-        #to put timestamp to next lowest hour
-        time = int(float(self.hrarr[0]))%3600
-        time = int(float(self.hrarr[0]))-time+3600
+        # to put timestamp to next lowest hour
+        time = int(float(self.hrarr[0])) % 3600
+        time = int(float(self.hrarr[0])) - time + 3600
         time = time - self.offset
 
-
-        #inserts all values from array to database
-        while(i<len(ar)):
-            com = 'UPDATE SET '+st+'='+str(ar[i])+'WHERE date = time'
+        # inserts all values from array to database
+        while (i < len(ar)):
+            com = 'UPDATE SET ' + st + '=' + str(ar[i]) + 'WHERE date = time'
             self.cursor.execute(com)
-            i = i+1
-            time = time+3600
-
+            i = i + 1
+            time = time + 3600
 
     def dbavger(self, ary):
-        timestamp = int(float(ary[0])%3600)
+        timestamp = int(float(ary[0]) % 3600)
         sampleRate = int(float(ary[1]))
         arrayIndex = 0
-        if(timestamp > 0):                                 # this is to ensure no partial hours are measured
-            arrayIndex = timestamp*sampleRate    # skip ahead to the first full hour of data
+        if (timestamp > 0):  # this is to ensure no partial hours are measured
+            arrayIndex = timestamp * sampleRate  # skip ahead to the first full hour of data
 
         counter = 0
         returnVal = []
@@ -112,30 +108,28 @@ class loadDataGUI():
 
         if (sampleRate == 1):
             type = "HR"
-            counter = 360           #the counter is the incoming array length, divided by the number of measurements in an hour
-            divisor = 360           #divisor is used to calculate the average later
+            counter = 360  # the counter is the incoming array length, divided by the number of measurements in an hour
+            divisor = 360  # divisor is used to calculate the average later
         elif (sampleRate == 4):
             type = "EDA"
             counter = 1440
             divisor = 1440
 
-        end = len(ary) - arrayIndex  #number of valid data indexes in list
+        end = len(ary) - arrayIndex  # number of valid data indexes in list
         leftOver = end % divisor
-        end = end-leftOver              #end is the final index of the measured data
-        leveler = (end-arrayIndex)%10   # sets the end to the last set or data that needs to be read
+        end = end - leftOver  # end is the final index of the measured data
+        leveler = (end - arrayIndex) % 10  # sets the end to the last set or data that needs to be read
         end = end - leveler
 
+        while (arrayIndex <= (end - divisor * 10)):  # outer loop counts up from 0 to 23
 
-        while (arrayIndex <= (end-divisor*10)):  # outer loop counts up from 0 to 23
-
-            while (counter > 0):             # inner loop counts down from counter to 0
+            while (counter > 0):  # inner loop counts down from counter to 0
                 sum += float(ary[arrayIndex])  # array index increments by ten so only a tenth
-                arrayIndex += 10             # of the values are collected
+                arrayIndex += 10  # of the values are collected
                 counter -= 1
             average = sum / divisor
-            returnVal.append(average)   #store the average for this hour in the return array
+            returnVal.append(average)  # store the average for this hour in the return array
             counter = divisor
             hours += 1
-
 
         self.commitdb(returnVal, type)
