091bf43675c4b5626a9a622a6a139e8d2b17cfc7
aabailey58 <alexander.bailey38@outlook.com>
2017-04-11 16:17:34 -0400

    Update loadusrdata.py

--- loadusrdata.py
+++ loadusrdata.py
@@ -17,6 +17,17 @@ class loadDataGUI():
 
         self.datawin.mainloop()
 
+        # an offset to put utc time into eastern time, 4 hours
+        self.offset = 3600*4
+
+        # establish db connection to be used
+        self.connection = pymysql.connect(host='localhost',
+                                          user='a',
+                                          password='a',
+                                          db='empaticareader')
+
+        self.cursor = self.connection.cursor()
+
     def close(self):
         self.datawin.destroy()
 
@@ -30,13 +41,6 @@ class loadDataGUI():
         self.accarr = self.activity.read().split("\n")
         self.arousalarr = self.arousal.read().split("\n")
 
-# establish db connection to be used
-        self.connection = pymysql.connect(host = 'localhost',
-                                      user = 'root',
-                                      password = 'a',
-                                      db = 'empaticareader')
-
-        self.cursor = self.connection.cursor()
 
 
 # call the methods to compile the data from the files
@@ -51,11 +55,12 @@ class loadDataGUI():
 # pull largest and thus most recent date from db
         self.cursor.exectute('SELECT MAX(date) FROM Data')
         last = self.cursor.fetchone()
+        last -= last(0)-self.offset
         i = 1
 # fill the days with no data with 0's
         while(dat< last):
             last = (last+3600*i)
-            stat = 'INSERT into Data (date, ACC, HR, EDA) VALUES('+last+', 0,0,0)'
+            stat = 'INSERT into Data (date, ACC, HR, EDA) VALUES('+str(last)+', 0,0,0)'
             self.cursor.execute(stat)
 
 
@@ -67,52 +72,55 @@ class loadDataGUI():
 #to put timestamp to next lowest hour
         time = int(self.hrarr[0])%3600
         time = int(self.hrarr[0])-time+3600
+        time = time - self.offset
 
 
 #inserts all values from array to database
         while(i<len(ar)):
-            com = 'INSERT into Data( Date,'+st+') VALUES ('+time+', '+ar[i]+')'
+            com = 'INSERT into Data( date,'+st+') VALUES ('+str(time)+', '+ar[i]+')'
             self.cursor.execute(com)
             i = i+1
             time = time+3600
-def dbavger(self, ary):
-    timestamp = ary[0]%3600
-    sampleRate = ary[1]
-    arrayIndex = 0
-    if(self.timestamp > 0):                                 #this is to ensure no partial hours are measured
-        self.arrayIndex = timestamp*sampleRate    #skip ahead to the first full hour of data
-
-    counter = 0
-    returnVal = []
-    type = ""
-    sum = 0
-    divisor = 1
-    average = 0
-    hours = 0
-
-    if (sampleRate == 1):
-        type = "HR"
-        counter = 360           #the counter is the incoming array length, divided by the number of measurements in an hour
-        divisor = 360           #divisor is used to calculate the average later
-    elif (sampleRate == 4):
-        type = "EDA"
-        counter = 1440
-        divisor = 1440
-
-    end = len(ary) - arrayIndex   #this is to remove any partial hours at the end of the sample
-    leftOver = end%divisor
-    end = end-leftOver              #end is the final index of the measured data
-
-    while (arrayIndex < end):  # outer loop counts up from 0 to 23
-
-        while (counter > 0):             # inner loop counts down from counter to 0
-            sum += ary[arrayIndex]  # array index increments by ten so only a tenth
-            arrayIndex += 10             # of the values are collected
-            counter -= 1
-        average = sum / divisor
-        returnVal.append(average)   #store the average for this hour in the return array
-        counter = divisor
-        hours += 1
 
 
-    self.commitdb(returnVal, type)            
+    def dbavger(self, ary):
+        timestamp = ary[0]%3600
+        sampleRate = ary[1]
+        arrayIndex = 0
+        if(timestamp > 0):                                 # this is to ensure no partial hours are measured
+            arrayIndex = timestamp*sampleRate    # skip ahead to the first full hour of data
+
+        counter = 0
+        returnVal = []
+        type = ""
+        sum = 0
+        divisor = 1
+        average = 0
+        hours = 0
+
+        if (sampleRate == 1):
+            type = "HR"
+            counter = 360           #the counter is the incoming array length, divided by the number of measurements in an hour
+            divisor = 360           #divisor is used to calculate the average later
+        elif (sampleRate == 4):
+            type = "EDA"
+            counter = 1440
+            divisor = 1440
+
+        end = len(ary) - arrayIndex   #this is to remove any partial hours at the end of the sample
+        leftOver = end % divisor
+        end = end-leftOver              #end is the final index of the measured data
+
+        while (arrayIndex < end):  # outer loop counts up from 0 to 23
+
+            while (counter > 0):             # inner loop counts down from counter to 0
+                sum += ary(arrayIndex)  # array index increments by ten so only a tenth
+                arrayIndex += 10             # of the values are collected
+                counter -= 1
+            average = sum / divisor
+            returnVal.append(average)   #store the average for this hour in the return array
+            counter = divisor
+            hours += 1
+
+
+        self.commitdb(returnVal, type)
