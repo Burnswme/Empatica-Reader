7b1f24cab70ef8cedbb07ae246d34861a19b31b9
aabailey58 <alexander.bailey38@outlook.com>
2017-04-25 17:40:17 -0400

    Add files via upload

--- UserGUI.py
+++ UserGUI.py
@@ -1,5 +1,6 @@
 #import tkinter and pymysql for database interaction
 from tkinter import *
+from tkinter import ttk
 import sqlite3
 import datetime
 from PIL import Image, ImageTk
@@ -16,17 +17,15 @@ class UsrGUI():
     cursor = connection.cursor()
 
     # pull most recent date to start from
-    com = 'SELECT max(date) from data;'
+    com = 'SELECT max(date) from data'
     cursor.execute(com)
-
     pull = cursor.fetchone()
     startdate = int(pull[0])
     #set recdate to one week ago from the most recent date
     recdate = startdate - startdate % DAY
     recdate = recdate - 518400
-
     ti = recdate - OFFSET
-    # get data form baselines file then store in variables for clarity
+
     # baselines
     arrbase = 0
     accbase = 0
@@ -43,6 +42,15 @@ class UsrGUI():
     arrimg = ''
     accimg = ''
     hrimg = ''
+    # max and min
+    arrmax = 0
+    arrmin = 0
+
+    accmax = 0
+    accmin = 0
+
+    hrmax = 0
+    hrmin = 0
 
     # set in setcolor method, used in setalert
     ACC = 0
@@ -61,21 +69,29 @@ class UsrGUI():
         self.metric = metric
 
         self.rootu = Toplevel()
-        self.rootu.geometry('1200x600')
+        self.rootu.geometry('1200x625')
 
         self.rootu.title('Empatica Reader')
 
 # creates canvas to display data
-        self.daysCanvas = Canvas(self.rootu, width = 500, height = 600, bg = 'red')
-        self.daysCanvas.grid(row=1, column=0, rowspan=7, columnspan = 25, stick = 'nsew')
+        self.daysCanvas = Canvas(self.rootu, width = 500, height = 600)
+        self.daysCanvas.grid(row=0, column=0, rowspan=7, columnspan = 25, stick = 'nsew')
 
-        self.dayLbl = Label(self.rootu, width = self.w, text = 'Day').grid(row = 0, column = 0)
+        self.dayLbl = Label(self.daysCanvas, width = self.w, text = 'Day').grid(row = 0, column = 0)
 
 # buttons to select which metric is displayed
-        self.selhr = Button(self.rootu, text = 'Heart rate', command = self.sethrdisplay).grid(row = 1,column = 26)
-        self.selacc = Button(self.rootu, text = 'Activity (default)', command = self.setaccdisplay).grid(row = 2,column = 26)
-        self.seleda = Button(self.rootu, text = 'Arousal', command = self.setedadisplay).grid(row = 3,column = 26)
+        self.selhr = ttk.Button(self.rootu, text = 'Heart rate', command = self.sethrdisplay).grid(row = 0,column = 26)
+        self.selacc = ttk.Button(self.rootu, text = 'Activity (default)', command = self.setaccdisplay).grid(row = 1,column = 26)
+        self.seleda = ttk.Button(self.rootu, text = 'Arousal', command = self.setedadisplay).grid(row = 2,column = 26)
 
+# key for the colors
+        self.blue = Label(self.rootu, bg = '#2B5E9F', text = 'very low reading').grid(row = 7, column = 26)
+        self.lblue = Label(self.rootu, bg = '#6F9FDC', text = 'low reading').grid(row = 6, column = 26)
+        self.green = Label(self.rootu, bg = '#3ED748', text = 'good reading').grid(row =5, column = 26)
+        self.orange = Label(self.rootu, bg = '#F2B43A', text = 'high reading').grid(row = 4, column = 26)
+        self.red = Label(self.rootu, bg = '#FF0000', text = 'very high reading').grid(row = 3, column = 26)
+
+        # get data form baselines file then store in variables for clarity
         self.baselines = open('baselines.txt', 'r').read().split('\n')
         # baselines
         self.arrbase = float(self.baselines[0])
@@ -93,8 +109,17 @@ class UsrGUI():
         self.arrimg = self.baselines[9]
         self.accimg = self.baselines[10]
         self.hrimg = self.baselines[11]
+        # max and min
+        self.accmax = float(self.baselines[12])
+        self.accmin = float(self.baselines[13])
 
-        # colored hour labels for canvas
+        self.hrmax = float(self.baselines[14])
+        self.hrmin = float(self.baselines[15])
+
+        self.arrmax = float(self.baselines[16])
+        self.arrmin = float(self.baselines[17])
+
+# colored hour labels for canvas
         self.displayMatrix = [[0 for x in range(24)]for y in range(7)]
 
         i = 0
@@ -118,37 +143,37 @@ class UsrGUI():
         i = 0
         while i <=6:
             d = datetime.datetime.fromtimestamp(dnum).strftime('%m-%d') # convert unix timestamp into readable date
-            self.datematrix[i] = Label(self.daysCanvas, text = d, width = self.w)\
+            self.datematrix[i] = Label(self.daysCanvas, text = d, width = self.w, height = self.w)\
                 .grid(row = i+1, column = 0, stick = 'nsew')
             i += 1
             dnum += 86400
 
 # hour labels for top, if statement to designate this as a block for clarity while coding
         if True:
-            twelveA = Label(self.rootu, text = '12:00',width = self.w).grid(row = 0,column = 1)
-            oneA = Label(self.rootu, text = '1:00',width = self.w ).grid(row = 0,column = 2)
-            twoA = Label(self.rootu, text = '2:00',width = self.w ).grid(row = 0,column = 3)
-            threeA = Label(self.rootu, text = '3:00',width = self.w ).grid(row = 0,column = 4)
-            fourA = Label(self.rootu, text = '4:00',width = self.w ).grid(row = 0,column = 5)
-            fiveA = Label(self.rootu, text = '5:00',width = self.w ).grid(row = 0,column = 6)
-            sixA = Label(self.rootu, text = '6:00',width = self.w ).grid(row = 0,column = 7)
-            sevenA = Label(self.rootu, text = '7:00',width = self.w ).grid(row = 0,column = 8)
-            eightA = Label(self.rootu, text = '8:00',width = self.w ).grid(row = 0,column = 9)
-            nineA = Label(self.rootu, text = '9:00',width = self.w ).grid(row = 0,column = 10)
-            tenA = Label(self.rootu, text = '10:00',width = self.w ).grid(row = 0,column = 11)
-            elevenA = Label(self.rootu, text = '11:00',width = self.w ).grid(row = 0,column = 12)
-            twelveP = Label(self.rootu, text = '12:00',width = self.w ).grid(row = 0,column = 13)
-            oneP = Label(self.rootu, text = '1:00',width = self.w ).grid(row = 0,column = 14)
-            twoP = Label(self.rootu, text = '2:00',width = self.w ).grid(row = 0,column = 15)
-            threeP = Label(self.rootu, text = '3:00',width = self.w ).grid(row = 0,column = 16)
-            fourP = Label(self.rootu, text = '4:00',width = self.w ).grid(row = 0,column = 17)
-            fiveP = Label(self.rootu, text = '5:00',width = self.w ).grid(row = 0,column = 18)
-            sixP = Label(self.rootu, text = '6:00',width = self.w ).grid(row = 0,column = 19)
-            sevenP = Label(self.rootu, text = '7:00',width = self.w ).grid(row = 0,column = 20)
-            eightP = Label(self.rootu, text = '8:00',width = self.w ).grid(row = 0,column = 21)
-            nineP = Label(self.rootu, text = '9:00',width = self.w ).grid(row = 0,column = 22)
-            tenP = Label(self.rootu, text = '10:00',width = self.w ).grid(row = 0,column = 23)
-            elevenP = Label(self.rootu, text = '11:00',width = self.w ).grid(row = 0,column = 24)
+            twelveA = Label(self.daysCanvas, text = '12:00',width = self.w).grid(row = 0,column = 1)
+            oneA = Label(self.daysCanvas, text = '1:00',width = self.w ).grid(row = 0,column = 2)
+            twoA = Label(self.daysCanvas, text = '2:00',width = self.w ).grid(row = 0,column = 3)
+            threeA = Label(self.daysCanvas, text = '3:00',width = self.w ).grid(row = 0,column = 4)
+            fourA = Label(self.daysCanvas, text = '4:00',width = self.w ).grid(row = 0,column = 5)
+            fiveA = Label(self.daysCanvas, text = '5:00',width = self.w ).grid(row = 0,column = 6)
+            sixA = Label(self.daysCanvas, text = '6:00',width = self.w ).grid(row = 0,column = 7)
+            sevenA = Label(self.daysCanvas, text = '7:00',width = self.w ).grid(row = 0,column = 8)
+            eightA = Label(self.daysCanvas, text = '8:00',width = self.w ).grid(row = 0,column = 9)
+            nineA = Label(self.daysCanvas, text = '9:00',width = self.w ).grid(row = 0,column = 10)
+            tenA = Label(self.daysCanvas, text = '10:00',width = self.w ).grid(row = 0,column = 11)
+            elevenA = Label(self.daysCanvas, text = '11:00',width = self.w ).grid(row = 0,column = 12)
+            twelveP = Label(self.daysCanvas, text = '12:00',width = self.w ).grid(row = 0,column = 13)
+            oneP = Label(self.daysCanvas, text = '1:00',width = self.w ).grid(row = 0,column = 14)
+            twoP = Label(self.daysCanvas, text = '2:00',width = self.w ).grid(row = 0,column = 15)
+            threeP = Label(self.daysCanvas, text = '3:00',width = self.w ).grid(row = 0,column = 16)
+            fourP = Label(self.daysCanvas, text = '4:00',width = self.w ).grid(row = 0,column = 17)
+            fiveP = Label(self.daysCanvas, text = '5:00',width = self.w ).grid(row = 0,column = 18)
+            sixP = Label(self.daysCanvas, text = '6:00',width = self.w ).grid(row = 0,column = 19)
+            sevenP = Label(self.daysCanvas, text = '7:00',width = self.w ).grid(row = 0,column = 20)
+            eightP = Label(self.daysCanvas, text = '8:00',width = self.w ).grid(row = 0,column = 21)
+            nineP = Label(self.daysCanvas, text = '9:00',width = self.w ).grid(row = 0,column = 22)
+            tenP = Label(self.daysCanvas, text = '10:00',width = self.w ).grid(row = 0,column = 23)
+            elevenP = Label(self.daysCanvas, text = '11:00',width = self.w ).grid(row = 0,column = 24)
 
     def sethrdisplay(self):
         self.rootu.destroy()
@@ -170,11 +195,10 @@ class UsrGUI():
         # set unix timestamp from day/hour
         work = True
         # pull necessary data
-        com = ('SELECT ACC, HR, EDA from data WHERE date = '+str(self.ti)+';')
+        com = ('SELECT ACC, HR, EDA, ACCalert, HRalert, EDAalert from data WHERE date = '+str(self.ti)+';')
 
         try:
             self.cursor.execute(com)
-
 
         except:
             work = False
@@ -188,21 +212,27 @@ class UsrGUI():
             self.ACC = float(datatuple[0])
             self.HR = float(datatuple[1])
             self.EDA = float(datatuple[2])
+            ACCal = float(datatuple[3])
+            HRal = float(datatuple[4])
+            EDAal = float(datatuple[5])
 
-            # Activiy set
+
+            # Activity set
             if self.metric == 'ACC':
-                # low holds the lower threshold, half a value of half the threshold, high holds the high threshold
-                low = self.accbase - self.accbase*self.accthresh
-                half = (self.accbase*self.accthresh)/2
-                high = self.accbase + self.accbase*self.accthresh
+            # low holds the lower 25%, midd holds second 25%, high holds the higher 25%
+                quarter = (self.accmax - self.accmin)*.05
+                low = self.accmin + quarter
+                midd = low + quarter
+                high = midd + quarter
+
             # below lower is blue(with alert)
                 if self.ACC<low:
                     tcolor = '#2B5E9F'
             # from lower to half of threshold is light blue
-                elif self.ACC<low+half:
+                elif self.ACC<midd:
                     tcolor = '#6F9FDC'
             # within half of threshold is green
-                elif self.ACC<high-half:
+                elif self.ACC<self.accmax:
                     tcolor = '#3ED748'
             # from half to upper is orange
                 elif self.ACC<high:
@@ -211,7 +241,7 @@ class UsrGUI():
                 else:
                     tcolor = '#FF0000'
                 # check whether to set alert or not
-                if (self.ACC < low or self.ACC > high) and self.ACC != 0:
+                if (ACCal == 1) and self.ACC != 0:
                     temp = Image.open(self.accimg)
                     temp = temp.resize((40, 40), Image.ANTIALIAS)
                     self.image[x][y] = ImageTk.PhotoImage(temp)
@@ -220,28 +250,29 @@ class UsrGUI():
 
             # Heart rate set
             elif self.metric == 'HR':
-                # low holds the lower threshold, half a value of half the threshold, high holds the high threshold
-                low = self.hrbase - self.hrbase * self.hrthresh
-                half = (self.hrbase * self.hrthresh) / 2
-                high = self.hrbase + self.hrbase * self.hrthresh
+                # low holds the lower 25%, midd holds second 25%, high holds the higher 25%
+                quarter = (self.hrmax - self.hrmin)*.05
+                low = self.hrmin + quarter
+                midd = low + quarter
+                high = midd + quarter
 
                 # below lower is blue(with alert)
                 if self.HR < low:
                     tcolor = '#2B5E9F'
                     # from lower to half of threshold is light blue
-                elif self.HR < low + half:
+                elif self.HR < midd:
                     tcolor = '#6F9FDC'
                     # within half of threshold is green
-                elif self.HR < high - half:
+                elif self.HR < high:
                     tcolor = '#3ED748'
                     # from half to upper is orange
-                elif self.HR < high:
+                elif self.HR < self.hrmax:
                     tcolor = '#F2B43A'
                     # above upper is red (with alert)
                 else:
                     tcolor = '#FF0000'
                 # check whether to set alert
-                if (self.HR < low or self.HR > high) and self.HR != 0:
+                if (HRal == 1) and self.HR != 0:
                     temp = Image.open(self.hrimg)
                     temp = temp.resize((40, 40), Image.ANTIALIAS)
                     self.image[x][y] = ImageTk.PhotoImage(temp)
@@ -250,28 +281,30 @@ class UsrGUI():
 
             # Arousal set
             elif self.metric == 'EDA':
-                # low holds the lower threshold, half a value of half the threshold, high holds the high threshold
-                low = self.arrbase - self.arrbase * self.arrthresh
-                half = (self.arrbase * self.arrthresh) / 2
-                high = self.arrbase + self.arrbase * self.arrthresh
+                # low holds the lower 5% , midd second 5%, high holds the highest 5%
+                # we use 5% here because of how small the averages are for EDA data
+                perc = (self.arrmax - self.arrmin)*.015
+                low = self.arrmin + perc
+                midd = low + perc
+                high = midd + perc
 
                 # below lower is blue(with alert)
                 if self.EDA < low:
                     tcolor = '#2B5E9F'
                     # from lower to half of threshold is light blue
-                elif self.EDA < low + half:
+                elif self.EDA < midd:
                     tcolor = '#6F9FDC'
                     # within half of threshold is green
-                elif self.EDA < high - half:
+                elif self.EDA < high:
                     tcolor = '#3ED748'
                     # from half to upper is orange
-                elif self.EDA < high:
+                elif self.EDA < self.arrmax:
                     tcolor = '#F2B43A'
                     # above upper is red (with alert)
                 else:
                     tcolor = '#FF0000'
                 # check whether to set alert or not
-                if (self.EDA < low or self.EDA > high) and self.EDA != 0:
+                if (EDAal == 1) and self.EDA != 0:
                     temp = Image.open(self.arrimg)
                     temp = temp.resize((40, 40), Image.ANTIALIAS)
                     self.image[x][y] = ImageTk.PhotoImage(temp)
